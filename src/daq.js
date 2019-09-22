468c08758dd93bc88842e252b42d26eba935fe8f
Eunchurn Park <eunchurn.park@gmail.com>
2019-09-23 07:26:59 +0900

    data parsing

--- src/daq.js
+++ src/daq.js
@@ -13,35 +13,14 @@ server.on("error", err => {
 });
 
 server.on("message", data => {
-  // console.log(data)
   const parsedData = dataParse(data);
   parsedData.channel = parsedData.channel.map((obj, index) => {
     obj[index] = obj.data;
     delete obj.data;
     return obj;
   });
-  console.log(parsedData)
-  // redisClient.zaddData("DAQ", parsedData);
+  console.log(parsedData.channel)
 });
-
-setInterval(function() {
-  redisClient.exists("DAQ", (err, rexist) => {
-    if (rexist) {
-      redisClient.stackData("DAQ", "DAQRT", 1000); // cache to stack Redis Key, duration : 10ms
-    }
-  });
-}, 1000);
-
-const getDataAndEmit = async socket => {
-  try {
-    const res = await redisClient.client.get("DAQRT", (err, data) => {
-      // socket.emit("FromAPI", JSON.parse(data));
-      // MQTT sender
-    });
-  } catch (error) {
-    console.error(`Error: ${error}`);
-  }
-};
 
 server.bind(58432, () => {
   server.addMembership('234.5.6.7');
