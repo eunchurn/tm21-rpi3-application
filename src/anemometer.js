ef362aa30ae30801cc6b02b96b875f5a7030dc3b
Eunchurn Park <eunchurn.park@gmail.com>
2019-09-24 03:35:35 +0900

    netcat change

--- src/anemometer.js
+++ src/anemometer.js
@@ -1,37 +1,40 @@
 import net from "net";
 import "dotenv/config";
+import netcatClient from "netcat/client";
 import EventEmitter from "events";
 
 class anemoEvent extends EventEmitter {}
 const anemometer = new anemoEvent();
 
-const client = net.createConnection(
-  {
-    host: process.env.ANEMOMETER_HOST,
-    port: process.env.ANEMOMETER_PORT
-  },
-  () => {
-    console.log("connected to Anemometer server");
-  }
-);
-client.setNoDelay(true);
+netcatClient
+  .addr(process.env.ANEMOMETER_HOST)
+  .port(Number(process.env.ANEMOMETER_PORT))
+  .connect()
+  .on("data", console.log);
 
-client.on("data", data => {
-  console.log("received: ",
-    data.toString('utf8')
-      .replace(/\r/g, "")
-      .replace(/\n/g, "")
-      .split(/\,/g)
-  );
+// const client = net.createConnection(
+//   {
+//     host: process.env.ANEMOMETER_HOST,
+//     port: process.env.ANEMOMETER_PORT
+//   },
+//   () => {
+//     console.log("connected to Anemometer server");
+//   }
+// );
 
-  // if(i == 0)	wind_data.utc			= (int)tm_sec;
-  // if(i == 1)	wind_data.windDirection	= atoi(token);		// short
-  // if(i == 2)	wind_data.windSpeed		= atof(token);		// float
-  // if(i == 4)	wind_data.status		= atoi(token);
-  anemometer.emit("data", data);
-});
-client.on("end", () => {
-  console.log("disconnected from server");
-});
+// client.on("data", data => {
+//   console.log("received: ",
+//     data.toString('utf8')
+//   );
+
+//   // if(i == 0)	wind_data.utc			= (int)tm_sec;
+//   // if(i == 1)	wind_data.windDirection	= atoi(token);		// short
+//   // if(i == 2)	wind_data.windSpeed		= atof(token);		// float
+//   // if(i == 4)	wind_data.status		= atoi(token);
+//   anemometer.emit("data", data);
+// });
+// client.on("end", () => {
+//   console.log("disconnected from server");
+// });
 
 export default anemometer;
