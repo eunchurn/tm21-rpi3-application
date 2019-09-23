14bfb39f616e217f03031e0e577bfbc753597fcd
Eunchurn Park <eunchurn.park@gmail.com>
2019-09-24 03:17:56 +0900

    anemometer data encoding

--- src/anemometer.js
+++ src/anemometer.js
@@ -14,6 +14,7 @@ const client = net.createConnection(
     console.log("connected to Anemometer server");
   }
 );
+client.setNoDelay(true);
 
 client.on("data", data => {
   console.log("received: ",
