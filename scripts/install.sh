fb1acccaa6b30819c496d7b111f9c89172d44025
Eunchurn Park <eunchurn.park@gmail.com>
2019-11-07 05:14:22 +0900

    install systemd

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-scriptfile=/root/app/scripts/dku.sh
+scriptfile=/root/app/scripts/dku.service
 echo "Installing DKU monitoring application to system services target: $scriptfile"
-chmod +x $scriptfile
-cp $scriptfile /etc/init.d/dku
-update-rc.d dku defaults
-echo "Done"
-echo "[Start service] : /etc/init.d/dku start"
-echo "[Stop service] : /etc/init.d/dku stop"
-echo "[Restart service] : /etc/init.d/dku restart"
-echo "[Status] : /etc/init.d/dku status"
-echo "[Build service] : /etc/init.d/dku build"
-echo "[Logging App] : /etc/init.d/dku log"
+cp /root/app/scripts/dku.service /etc/systemd/system/dku.service
+systemctl enable dku
+echo "🦄 Done!"
+echo "✅[start] systemctl start dku"
+echo "❎[stop] systemctl stop dku"
+echo "🔧[restart] systemctl restart dku"
+echo "💡[status] systemctl status dku"
+echo "🔍[log] journalctl -u dku"
+echo "🗓[log following] journalctl -u dku -f"
