8ebaa1dfdcef987789cf18bd66c805dd025c80c8
Eunchurn Park <eunchurn.park@gmail.com>
2019-11-30 14:59:24 +0900

    install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,10 +4,10 @@ curl --insecure -o- \
 https://raw.githubusercontent.com/eunchurn/tm21-rpi3-application/master/scripts/dku.service \
 > /etc/systemd/system/dku.service
 systemctl enable dku
-echo "🦄 Done!"
-echo "✅[start] systemctl start dku"
-echo "❎[stop] systemctl stop dku"
-echo "🔧[restart] systemctl restart dku"
-echo "💡[status] systemctl status dku"
-echo "🔍[log] journalctl -u dku"
-echo "🗓[log following] journalctl -u dku -f"
+echo "🦄 All Done!"
+echo "✅   start: `systemctl start dku`"
+echo "❎    stop: `systemctl stop dku`"
+echo "🔧 restart: `systemctl restart dku`"
+echo "💡  status: `systemctl status dku`"
+echo "🔍     log: `journalctl -u dku`"
+echo "🗓 logging: `journalctl -u dku -f`"
